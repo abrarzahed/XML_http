76be3b02d64ec4421969908188ca5cf3545107b9
abrarzahed <abrarzahed6986@gmail.com>
2022-06-23 14:57:33 +0600

    completed

---
+++ app.js
@@ -0,0 +1,42 @@
+const getButton = document.getElementById("get");
+const sendButton = document.getElementById("send");
+
+const sendRequest = function (method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(data);
+
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+  });
+  return promise;
+};
+
+const getData = function () {
+  sendRequest("GET", "https://jsonplaceholder.typicode.com/todos/1").then(
+    (responseData) => {
+      console.log(responseData);
+    }
+  );
+};
+
+const sendData = function () {
+  sendRequest(
+    "POST",
+    "https://jsonplaceholder.typicode.com/posts",
+    JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    })
+  ).then((responseData) => {
+    console.log(responseData);
+  });
+};
+
+getButton.addEventListener("click", getData);
+sendButton.addEventListener("click", sendData);
